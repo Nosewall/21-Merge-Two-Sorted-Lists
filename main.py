3fbc04471112074e4b0296f1fdec3a2ff784ab2d
Nolan Nordwall <NolanNordwall@gmail.com>
2022-07-29 16:32:55 -0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        pass
+
+
+sol = Solution()
+l1 = [1, 2, 3]
+l2 = [1, 3, 4]
+sol.mergeTwoLists(l1, l2)
